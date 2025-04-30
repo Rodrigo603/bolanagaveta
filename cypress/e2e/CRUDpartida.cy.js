cf8680b93f49da3d1db3790231b2bf09ba0733e0
Rodrigo Dantas <rodrigo6031@gmail.com>
2025-04-30 16:26:27 -0300

    correção de nome de arquivo

--- cypress/e2e/CRUDpartida.cy.js
+++ cypress/e2e/CRUDpartida.cy.js
@@ -1,11 +1,13 @@
 Cypress.Commands.add('deleteUsers', () => {
     return cy.exec('python delete_users.py', { failOnNonZeroExit: false }).then((result) => {
-      console.log(result.stdout);
-      if (result.stderr) {
-        console.error(result.stderr);
-      }
+        console.log(result.stdout); 
+        if (result.stderr) {
+            console.error(result.stderr);
+        } else {
+            console.log('Usuários excluídos com sucesso');
+        }
     });
-  });
+});
 
 
 Cypress.Commands.add('deleteCompeticoes', () => {
@@ -18,17 +20,7 @@ Cypress.Commands.add('deleteCompeticoes', () => {
 });
 
 
-Cypress.Commands.add('deleteTimes', () => {
-    return cy.exec('python delete_times.py', { failOnNonZeroExit: false }).then((result) => {
-      console.log(result.stdout);
-      if (result.stderr) {
-        console.error(result.stderr);
-      }
-    });
-});
-
-
-Cypress.Commands.add('signinGerenciador', () => {
+  Cypress.Commands.add('signinGerenciador', () => {
     cy.visit('/');
     cy.get('.nav-links > :nth-child(1) > a').click();
     cy.get('#tipo_usuario');
@@ -57,79 +49,153 @@ Cypress.Commands.add('criarCompeticao', () => {
 });
 
 
+Cypress.Commands.add('editarCompeticao', () => {
+    cy.get('.card-actions > a.btn').click();
+    cy.get('#nome').clear();
+    cy.get('#nome').type('Edição Cypress');
+    cy.get('#numero_de_times').clear();
+    cy.get('#numero_de_times').type('6');
+    cy.get('#local').clear();
+    cy.get('#local').type('Olinda');
+    cy.get('button.btn').click();
+
+});
+
+
 Cypress.Commands.add('criarTime', () => {
-  cy.get('.card-actions > a.btn > .fas').click();
-  cy.url().then((url) => {
-    const regex = /editar(\d+)\/editar/;
-    const match = url.match(regex);
-
-    if (match) {
-        const competicaoId = match[1];
-        cy.log('ID da competição:', competicaoId);
-
-        cy.get(`[href="/competicao/${competicaoId}/editar_times/"]`).click();
-    } else {
-        throw new Error('ID da competição não encontrado na URL!');
-    }
-});
-  cy.get('#nome').type('time Cypress');
-  cy.get('.form-actions > .btn').click();
-  cy.wait(1000);
-});
-
-
-Cypress.Commands.add('excluirTime', () => {
-  cy.get('.team-delete-form > .btn').click();
-  cy.wait(1000);
-});
+    cy.get('.card-actions > a.btn > .fas').click();
+    cy.url().then((url) => {
+      const regex = /editar(\d+)\/editar/;
+      const match = url.match(regex);
+  
+      if (match) {
+          const competicaoId = match[1];
+          cy.log('ID da competição:', competicaoId);
+  
+          cy.get(`[href="/competicao/${competicaoId}/editar_times/"]`).click();
+      } else {
+          throw new Error('ID da competição não encontrado na URL!');
+      }
+  });
+    cy.get('#nome').type('time Cypress');
+    cy.get('.form-actions > .btn').click();
+  });
 
 
-Cypress.Commands.add('criarTimeRepetido', () => {
-  cy.get('#nome').type('time Cypress');
-  cy.get('.form-actions > .btn').click();
-  cy.wait(1000);
-});
+Cypress.Commands.add('criarTime2', () => {
+    cy.get('#nome').type('time Cypress2');
+    cy.get('.form-actions > .btn').click();
+  });
+  
 
-
-describe('CRUD de times', () => {
+Cypress.Commands.add('criarPartida', () => {
+    cy.get('.btn-secondary').click();
+    cy.url().then((url) => {
+        const regex = /editar(\d+)\/editar/;
+        const match = url.match(regex);
     
-      
+        if (match) {
+            const competicaoId = match[1];
+            cy.log('ID da competição:', competicaoId);
+    
+            cy.get(`[href="/competicao/${competicaoId}/partidas/"]`).click();
+        } else {
+            throw new Error('ID da competição não encontrado na URL!');
+        }
+    });
+    cy.get('#time_casa');
+    cy.get('select[name="time_casa"]').select('time Cypress');
+    cy.get('#time_visitante');
+    cy.get('select[name="time_visitante"]').select('time Cypress2');
+    cy.get('#data').type('2025-04-29');
+    cy.get('#hora').type('16:00');
+    cy.get('form > .form-actions > .btn').click();
+    cy.wait(1500);
+
+});
+
+Cypress.Commands.add('criarPartidaIncompleta', () => {
+    cy.get('.btn-secondary').click();
+    cy.url().then((url) => {
+        const regex = /editar(\d+)\/editar/;
+        const match = url.match(regex);
+    
+        if (match) {
+            const competicaoId = match[1];
+            cy.log('ID da competição:', competicaoId);
+    
+            cy.get(`[href="/competicao/${competicaoId}/partidas/"]`).click();
+        } else {
+            throw new Error('ID da competição não encontrado na URL!');
+        }
+    });
+    cy.get('#time_casa');
+    cy.get('select[name="time_casa"]').select('time Cypress');
+    cy.get('#time_visitante');
+    cy.get('select[name="time_visitante"]').select('time Cypress2');
+    cy.get('#hora').type('16:00');
+    cy.get('form > .form-actions > .btn').click();
+    cy.wait(1500);
+});
+
+
+Cypress.Commands.add('editarPartida', () => {
+    cy.get('a[href^="/partida/"][href$="/editar/"]').first() 
+      .invoke('attr', 'href')
+      .then((href) => {
+        const id = href.split('/')[2];
+        cy.log('ID capturado:', id);
+        cy.get(`[href="/partida/${id}/editar/"]`).click(); 
+        cy.get('#data').type('2025-05-03');
+        cy.get('button.btn').click();
+        cy.wait(1000);
+      });
+  });
+  
+
+describe('CRUD de partidas', () => {
 
     beforeEach(() => {
         cy.deleteUsers()
           .then(() => cy.deleteCompeticoes())
-          .then(() => cy.deleteTimes())
           .then(() => {
               cy.clearCookies();
               cy.clearLocalStorage();
               cy.visit('/');
-          });
     });
+});
 
-    
-    it('Cenario 1: Criar o time e visualizá-lo com sucesso.', () => {
+
+    it('Cenario 1: Criação de partida bem sucedida', () => {
         cy.signinGerenciador();
         cy.loginGerenciador();
         cy.criarCompeticao();
+        cy.editarCompeticao();
         cy.criarTime();
+        cy.criarTime2();
+        cy.criarPartida();
     });
 
 
-    it('Cenario 2: : Cadastro de time já existente', () => {
+    it('Cenario 2: Criação de partida com dados incompletos' , () => {
         cy.signinGerenciador();
         cy.loginGerenciador();
         cy.criarCompeticao();
+        cy.editarCompeticao();
         cy.criarTime();
-        cy.criarTimeRepetido();
-    });
-    
-    
-    it('Cenario 3: Excluir Time', () => {
-       cy.signinGerenciador();
-       cy.loginGerenciador();
-       cy.criarCompeticao();
-       cy.criarTime();
-       cy.excluirTime();
+        cy.criarTime2();
+        cy.criarPartidaIncompleta();
     });
 
+
+    it('Cenario 3: Editar uma partida', () => {
+        cy.signinGerenciador();
+        cy.loginGerenciador();
+        cy.criarCompeticao();
+        cy.editarCompeticao();
+        cy.criarTime();
+        cy.criarTime2();
+        cy.criarPartida();
+        cy.editarPartida();
+    });
 });
